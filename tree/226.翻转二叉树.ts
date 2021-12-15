01c6db51b99dbb762489a454b8693cf1251da14a
lijunfeng <junfeng.li@hand-china.com>
2021-12-15 20:05:17 +0800

    226.翻转二叉树

--- tree/226.翻转二叉树.ts
+++ tree/226.翻转二叉树.ts
@@ -1,3 +1,4 @@
+export default {};
 /*
  * @lc app=leetcode.cn id=226 lang=typescript
  *
@@ -5,19 +6,17 @@
  */
 
 // @lc code=start
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
- */
+
+class TreeNode {
+  val: number
+  left: TreeNode | null
+  right: TreeNode | null
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+  }
+}
 
 function invertTree(root: TreeNode | null): TreeNode | null {
   if (!root) {
