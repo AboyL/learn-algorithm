20b130dc969d650238254c51905757f36c8052d1
lijunfeng <junfeng.li@hand-china.com>
2022-01-09 17:28:57 +0800

    215.数组中的第k个最大元素

---
+++ Heap/215.数组中的第k个最大元素.test.ts
@@ -0,0 +1,128 @@
+export default {}
+/*
+ * @lc app=leetcode.cn id=215 lang=typescript
+ *
+ * [215] 数组中的第K个最大元素
+ */
+
+// @lc code=start
+
+
+// 构建堆，然后直接进行弹出来
+
+// 通过堆来解决
+function swap<T = number>(arr: T[], a: number, b: number) {
+  const t: T = arr[a]
+  arr[a] = arr[b]
+  arr[b] = t
+}
+
+const compare = (a: number, b: number) => {
+  return a < b
+}
+
+class MaxHeap {
+  private data: number[] = []
+  private count: number = 0;
+
+  size() {
+    return this.count
+  }
+
+  isEmpty() {
+    return this.count === 0
+  }
+
+  getData() {
+    return this.data
+  }
+
+  heapify(arr: number[]) {
+    this.data = []
+    this.count = arr.length
+    if (arr.length <= 1) {
+      this.data = arr
+      return arr
+    }
+    arr.forEach((v, i) => {
+      this.data[i] = v
+    })
+    for (let i = Math.floor((arr.length - 2) / 2); i >= 0; i--) {
+      this.shiftDown(i)
+    }
+  }
+
+  // 对叶子节点进行操作
+  private shiftUp(k: number) {
+    // 对元素位置进行调整
+    // 对于堆 永远有父节点大于子节点
+    // 对子节点i 有 parent=Math.floor((i-1)/2) 
+    // 对父节点i 有 left child=i*2+1  right child =i*2+2
+    // 进行替换位置的时候 需要对比的初始值为 子节点 i= this.count
+    const pIndex = Math.floor((k - 1) / 2)
+    if (k > 0 && compare(this.data[pIndex], this.data[k])) {
+      // 进行位置的交换
+      swap(this.data, k, pIndex)
+      this.shiftUp(pIndex)
+    }
+  }
+
+  // 添加元素
+  insert(item: number) {
+    // 注意 data[0] 应该永远是undefined 便于计算
+    this.data[this.count++] = item
+    // 调整位置
+    this.shiftUp(this.count - 1)
+  }
+
+  // 对非叶子节点进行操作
+  private shiftDown(k: number) {
+    // 向下进行比较
+    const leftC = this.data[k * 2 + 1]
+    const rightC = this.data[k * 2 + 2]
+    const current = this.data[k]
+    if (leftC === undefined && rightC === undefined) {
+      return
+    }
+    const compareIndex = !compare(leftC, rightC) ? k * 2 + 1 : k * 2 + 2
+    if (compare(current, this.data[compareIndex])) {
+      // 进行位置的交换
+      swap(this.data, k, compareIndex)
+      this.shiftDown(compareIndex)
+    }
+  }
+
+  // 取出元素 必定为最大的元素
+  extractTarget(): number {
+
+    if (this.data.length === 0) {
+      throw new Error('没有元素了')
+    }
+    // 先跟最后一个元素调换一个位置再逐步进行下走
+    swap(this.data, 0, --this.count)
+    const target = this.data.pop()
+    this.shiftDown(0)
+    return target!
+  }
+}
+
+function findKthLargest(nums: number[], k: number): number {
+  const maxHeap = new MaxHeap()
+  maxHeap.heapify(nums)
+  let maxK = -(10 ** 7)
+  while (k > 0 && !maxHeap.isEmpty()) {
+    let max = maxHeap.extractTarget()
+    maxK = max
+    k--
+  }
+
+  return maxK
+};
+
+test('findKthLargest', () => {
+  expect(
+    findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4)
+  ).toEqual(4)
+})
+// @lc code=end
+
